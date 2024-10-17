ca5cec1d46d453ab58822537b825b29522839109
Samrita Shetty <SamritaSShetty@gmail.com>
2024-10-17 04:00:38 -0400

    Base contact the developers

--- CS_2340_team19P2/settings.py
+++ CS_2340_team19P2/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wrapped.apps.WrappedConfig',
-    'contactDevelopers.apps.ContactdevelopersConfig'
 ]
 
 MIDDLEWARE = [
